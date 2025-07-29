70ddc3e5d1b1cc0f58e7a4a445acef1f5a065186
lovepreet75way <lovepreet@75way.com>
2025-07-29 19:03:55 +0530

    created the structure for comment post and like and set up basic route

--- backend/src/app/routes.ts
+++ backend/src/app/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
+import React from 'react';
 import userRoutes from './user/user.route';
 // import similarly your post, comment, like routes when implemented
 
 const router = Router();
 
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
-// router.use('/comments', commentRoutes);
-// router.use('/likes', likeRoutes);
 
 export default router;
